21e7670a1f9106d3658823b054b8e3c1da470936
CarelessDeveloper <56518813+CarelessDeveloper@users.noreply.github.com>
2019-11-25 15:04:44 +0100

    Add files via upload

--- utilities/setup.py
+++ utilities/setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
-    url='https://github.com/jladan/package_demo',
-    author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
+    name='utilities',
+    url='https://github.com/CarelessDeveloper/utilities.git',
+    author='Olav Onstad',
+    author_email='CarelessDeveloper@hotmail.com',
     # Needed to actually package something
-    packages=['measure'],
+    packages=['utilities'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['os'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
     license='MIT',
-    description='An example of a python package from pre-existing code',
+    description='A package which helps you to style you cmd window easier',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
